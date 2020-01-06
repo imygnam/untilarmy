e2895769e13bcf2000f3496bb3123af38338e31f
imygnam <imygnam@gmail.com>
2020-01-06 23:53:29 +0900

    kiki

--- plz/app/src/main/java/com/example/plz/GetBJ.java
+++ plz/app/src/main/java/com/example/plz/GetBJ.java
@@ -26,11 +26,15 @@ public class GetBJ {
         t = 0;
         new Description().execute();
 
+        Log.d("문제수 찾는중", "시작" + t);
         while(true){
             if(t == 1) break;
             for(int q = 0; q < 100; q++){
+
             }
+            Log.d("반복", ""+t);
         }
+        Log.d("문제수 찾는중", "끝");
         return bj;
     }
 
@@ -45,6 +49,7 @@ public class GetBJ {
 
                 Elements titles = doc.select("td a"); //필요한 녀석만 꼬집어서 지정
 
+                Log.d("엘레멘트", titles+"");
 
                 if(titles.toString() != null) {
                     String str = titles.toString();
@@ -53,18 +58,23 @@ public class GetBJ {
                     for(; str.charAt(i) != '>';i++){
 
                     }
+                    Log.d("문제수 찾는중", ""+str.charAt(i));
                     for(i++;str.charAt(i) != '<';i++){
+                        Log.d("문제수 찾는중", ""+bj);
                         bj += str.charAt(i);
                     }
 
                 }
                 else bj = null;
+                Log.d("문제수 찾는중", "끌");
                 t = 1;
 
             } catch (IOException e) {
                 e.printStackTrace();
+                Log.d("문제수 찾는중", "아웃");
                 t = 1;
             }
+            Log.d("문제수 찾는중", "끝자락" + t);
             t = 1;
             return null;
         }
